07dc956554d92d203e4094caa3d10a4936008b94
Aja Hammerly <aja.hammerly+github@gmail.com>
2016-05-04 16:38:03 -0500

    Telegram bot WIP

--- blog/telegram-bot/bot.rb
+++ blog/telegram-bot/bot.rb
@@ -20,68 +20,72 @@ TELEGRAM_BOT_NAME = "RedditCuteBot"
 REDDIT_CLIENT_ID  = ENV["REDDIT_CLIENT_ID"]
 REDDIT_SECRET     = ENV["REDDIT_SECRET"]
 
-SUPPORTED_REDDITS = [
-  "aww",
-  "babygoats",
-  "batty",
-  "catbellies",
-  "catsinboxes",
-  "cute",
-  "daww",
-  "ferrets",
-  "gifsofotters",
-  "goats",
-  "husky",
-  "kittens",
-  "otters",
-  "puppies",
-  "raccoons",
-  "roombaww",
-  "seals",
-  "teefies",
-  "thecattrapisworking"
-]
+class CuteBot
+  SUPPORTED_REDDITS = [
+    "aww",
+    "babygoats",
+    "batty",
+    "catbellies",
+    "catsinboxes",
+    "cute",
+    "daww",
+    "ferrets",
+    "gifsofotters",
+    "goats",
+    "husky",
+    "kittens",
+    "otters",
+    "puppies",
+    "raccoons",
+    "roombaww",
+    "seals",
+    "teefies"
+  ]
 
-DOMAIN_WHITELIST = ["imgur", "instagram", "gfycat"]
+  DOMAIN_WHITELIST = ["imgur", "instagram", "gfycat"]
 
-
-@r = Redd.it(:userless, REDDIT_CLIENT_ID, REDDIT_SECRET)
-
-def send_message(bot, message, text)
-  bot.api.send_message(chat_id: message.chat.id, text: text)
-end
-
-def good_domain?(domain)
-  DOMAIN_WHITELIST.any? { |d| domain.include?(d) }
-end
-
-def post_cutes(bot, msg, subreddit)
-  send_message(bot, msg, "Querying r/#{subreddit}")
-
-  cutes = @r.get_hot(subreddit, :limit => 3)
-  cutes.each do |cute|
-    puts cute.domain
-    next unless good_domain?(cute.domain)
-    send_message(bot, msg, cute.url)
+  def initialize
+    @r = Redd.it(:userless, REDDIT_CLIENT_ID, REDDIT_SECRET)
   end
-end
 
-Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
-  bot.listen do |msg|
-    case msg.text
-    when '/start'
-      send_message(bot, msg, "Hello! To get cute type '/cute <reddit name>'")
-    when '/cute'
-      subreddit = SUPPORTED_REDDITS.shuffle.first
-      post_cutes(bot, msg, subreddit)
-    else
-      subreddit = msg.text[1..-1].downcase
+  def send_message(bot, message, text)
+    bot.api.send_message(chat_id: message.chat.id, text: text)
+  end
 
-      puts subreddit
+  def good_domain?(domain)
+    DOMAIN_WHITELIST.any? { |d| domain.include?(d) }
+  end
 
-      if SUPPORTED_REDDITS.include?(subreddit) then
+  def post_cutes(bot, msg, subreddit)
+    send_message(bot, msg, "Querying r/#{subreddit}")
+
+    cutes = @r.get_hot(subreddit, :limit => 3)
+    cutes.each do |cute|
+      puts cute.domain
+      next unless good_domain?(cute.domain)
+      send_message(bot, msg, cute.url)
+    end
+  end
+
+  Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
+    bot.listen do |msg|
+      case msg.text
+      when '/start'
+        send_message(bot, msg, "Hello! To get cute type '/cute <reddit name>'")
+      when '/cute'
+        subreddit = SUPPORTED_REDDITS.shuffle.first
         post_cutes(bot, msg, subreddit)
+      else
+        subreddit = msg.text[1..-1].downcase
+
+        puts subreddit
+
+        if SUPPORTED_REDDITS.include?(subreddit) then
+          post_cutes(bot, msg, subreddit)
+        end
       end
     end
   end
+end
+
 end
