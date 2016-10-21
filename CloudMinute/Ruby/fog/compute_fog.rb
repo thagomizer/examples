1bac880d0906f9bf4f8977c478f2969db16c8521
Aja Hammerly <ajahammerly@google.com>
2016-10-21 13:51:36 -0700

    Update fog compute script to work properly

--- CloudMinute/Ruby/fog/compute_fog.rb
+++ CloudMinute/Ruby/fog/compute_fog.rb
@@ -1,8 +1,26 @@
 require "fog/google"
 
 COMPUTE_ZONE = "us-central1-f"
+MACHINE_TYPE = "f1-micro"
 
 connection = Fog::Compute::Google.new
-flavor = connection.flavors.select { |f| f.zone == COMPUTE_ZONE and f.name == "f1-micro" }
 
-server = connection.servers.bootstrap(:name => "my-new-server", :flavor => flavor)
+disk = connection.disks.create(
+  :name => "new-disk",
+  :size_gb => 10,
+  :zone_name => COMPUTE_ZONE,
+  :source_image => "debian-8-jessie-v20161020")
+
+disk.wait_for { disk.ready? }
+
+server = connection.servers.create(
+  :name => "new-server",
+  :disks => [disk],
+  :machine_type => MACHINE_TYPE,
+  :private_key_path => File.expand_path("~/.ssh/id_rsa"),
+  :public_key_path => File.expand_path("~/.ssh/id_rsa.pub"),
+  :zone_name => COMPUTE_ZONE,
+  :user => ENV["USER"],
+  :tags => ["fog"])
+
+server.wait_for { server.ready? }
